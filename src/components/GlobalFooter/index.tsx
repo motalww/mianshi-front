0cc6a5c9a30b9782d4a795d46a0b0367cc46f726
www <2316754787@qq.com>
2025-01-02 22:47:29 +0800

    页面权限校验

--- src/components/GlobalFooter/index.tsx
+++ src/components/GlobalFooter/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import './index.css';
+import "./index.css";
+
 /**
  * 全局底部栏组件
  * @constructor
  */
 export default function GlobalFooter() {
-  const currentYear = new Date().getFullYear();
+    const currentYear = new Date().getFullYear();
+
   return (
     <div className="global-footer">
       <div>© {currentYear} 面试刷题平台</div>
--- src/layouts/BasicLayout/index.tsx
+++ src/layouts/BasicLayout/index.tsx
@@ -1,141 +1,153 @@
 "use client";
-
-import {GithubFilled, LogoutOutlined, SearchOutlined,} from "@ant-design/icons";
-import {ProLayout} from "@ant-design/pro-components";
-import {Dropdown, Input} from "antd";
+import {
+    GithubFilled,
+    LogoutOutlined,
+    SearchOutlined,
+} from "@ant-design/icons";
+import { ProLayout } from "@ant-design/pro-components";
+import { Dropdown, Input } from "antd";
 import React from "react";
 import Image from "next/image";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import GlobalFooter from "@/components/GlobalFooter";
-import './index.css'
-import {menus} from "../../../config/menu";
+import { menus } from "../../../config/menu";
+import { useSelector } from "react-redux";
+import { RootState } from "@/stores";
+// import getAccessibleMenus from "@/access/menuAccess";
+import "./index.css";
+import getAccessibleMenus from "@/access/menuAccess";
+
 /**
- * 搜索框
+ * 搜索条
  * @constructor
  */
 const SearchInput = () => {
-  return (
-    <div
-      key="SearchOutlined"
-      aria-hidden
-      style={{
-        display: "flex",
-        alignItems: "center",
-        marginInlineEnd: 24,
-      }}
-      onMouseDown={(e) => {
-        e.stopPropagation();
-        e.preventDefault();
-      }}
-    >
-      <Input
-        style={{
-          borderRadius: 4,
-          marginInlineEnd: 12,
-        }}
-        prefix={<SearchOutlined />}
-        placeholder="搜索题目"
-        variant="borderless"
-      />
-    </div>
-  );
+    return (
+        <div
+            key="SearchOutlined"
+            aria-hidden
+            style={{
+                display: "flex",
+                alignItems: "center",
+                marginInlineEnd: 24,
+            }}
+            onMouseDown={(e) => {
+                e.stopPropagation();
+                e.preventDefault();
+            }}
+        >
+            <Input
+                style={{
+                    borderRadius: 4,
+                    marginInlineEnd: 12,
+                }}
+                prefix={<SearchOutlined />}
+                placeholder="搜索题目"
+                variant="borderless"
+            />
+        </div>
+    );
 };
 
 interface Props {
-  children: React.ReactNode;
+    children: React.ReactNode;
 }
 
+/**
+ * 全局通用布局
+ * @param children
+ * @constructor
+ */
 export default function BasicLayout({ children }: Props) {
-  const pathname = usePathname();
-  return (
-    <div
-      id="basicLayout"
-      style={{
-        height: "100vh",
-        overflow: "auto",
-      }}
-    >
-      <ProLayout
-        title="面试w"
-        layout={"top"}
-        logo={
-          <Image
-            src="/assets/th.jpg"
-            height={32}
-            width={32}
-            alt={"面试刷题平台"}
-          />
-        }
-        location={{
-          pathname,
-        }}
-        avatarProps={{
-          src: "https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg",
-          size: "small",
-          title: "w",
-          render: (props, dom) => {
-            return (
-              <Dropdown
-                menu={{
-                  items: [
-                    {
-                      key: "logout",
-                      icon: <LogoutOutlined />,
-                      label: "退出登录",
-                    },
-                  ],
+    const pathname = usePathname();
+    // 当前登录用户
+    const loginUser = useSelector((state: RootState) => state.loginUser);
+
+    return (
+        <div
+            id="basicLayout"
+            style={{
+                height: "100vh",
+                overflow: "auto",
+            }}
+        >
+            <ProLayout
+                title="面试刷题平台"
+                layout="top"
+                logo={
+                    <Image
+                        src="/assets/th.jpg"
+                        height={32}
+                        width={32}
+                        alt="面试鸭刷题网站 - w"
+                    />
+                }
+                location={{
+                    pathname,
                 }}
-              >
-                {dom}
-              </Dropdown>
-            );
-          },
-        }}
-        actionsRender={(props) => {
-          if (props.isMobile) return [];
-          return [
-            <SearchInput key="search" />,
-            <a
-              key="github"
-              href="https://github.com/motalww/mianshi-front"
-              target="_blank"
+                avatarProps={{
+                    src: loginUser.userAvatar || "/assets/th.png",
+                    size: "small",
+                    title: loginUser.userName || "customer",
+                    render: (props, dom) => {
+                        return (
+                            <Dropdown
+                                menu={{
+                                    items: [
+                                        {
+                                            key: "logout",
+                                            icon: <LogoutOutlined />,
+                                            label: "退出登录",
+                                        },
+                                    ],
+                                }}
+                            >
+                                {dom}
+                            </Dropdown>
+                        );
+                    },
+                }}
+                actionsRender={(props) => {
+                    if (props.isMobile) return [];
+                    return [
+                        <SearchInput key="search" />,
+                        <a
+                            key="github"
+                            href="https://github.com/liyupi/mianshiya-next"
+                            target="_blank"
+                        >
+                            <GithubFilled key="GithubFilled" />
+                        </a>,
+                    ];
+                }}
+                headerTitleRender={(logo, title, _) => {
+                    return (
+                        <a>
+                            {logo}
+                            {title}
+                        </a>
+                    );
+                }}
+                // 渲染底部栏
+                footerRender={() => {
+                    return <GlobalFooter />;
+                }}
+                onMenuHeaderClick={(e) => console.log(e)}
+                //定义有哪些菜单
+                menuDataRender={() => {
+                    return getAccessibleMenus(loginUser, menus);
+                }}
+
+                // 定义了菜单项如何渲染
+                menuItemRender={(item, dom) => (
+                    <Link href={item.path || "/"} target={item.target}>
+                        {dom}
+                    </Link>
+                )}
             >
-              <GithubFilled key="GithubFilled" />
-            </a>,
-          ];
-        }}
-        headerTitleRender={(logo, title, _) => {
-          const defaultDom = (
-            <a>
-              {logo}
-              {title}
-            </a>
-          );
-          // if (typeof window === "undefined") return defaultDom;
-          if (document.body.clientWidth < 1400) {
-            return defaultDom;
-          }
-          if (_.isMobile) return defaultDom;
-          return <>{defaultDom}</>;
-        }}
-        //底部渲染
-        footerRender={() => {
-          return <GlobalFooter />
-        }}
-        //定义菜单
-        menuDataRender={() => {
-          return menus;
-        }}
-        //定义菜单项如何渲染
-        menuItemRender={(item, dom) => (
-          <Link href={item.path || "/"} target={item.target}>
-            {dom}
-          </Link>
-        )}
-      >
-        {children}
-      </ProLayout>
-    </div>
-  );
+                {children}
+            </ProLayout>
+        </div>
+    );
 }
